7bc996af0e22048359ff38653332a965c6904710
Constanza Latorre <constanzalatorrecid@gmail.com>
2023-11-20 19:48:01 -0300

    lunes

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,37 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [topRated, setTopRated] = useState([]);
+
   useEffect(() => {
     fetchTopRatedMovies()
       .then((data) => {
         setTopRated(data);
       })
-      .catch((err) => console.error(err));
-
+      .catch((err) => {
+        console.error('Error fetching top rated movies:', err);
+      });
   }, []);
+
   return (
-    <>
-  {console.log(topRated)}
-    </>
-  )
+    <div>
+      <h1>Top Rated Movies</h1>
+      <ul>
+        {topRated.map((movie) => (
+          <li key={movie.id}>
+            <h3>{movie.title}</h3>
+            <p>Release Date: {movie.release_date}</p>
+            <p>Rating: {movie.vote_average}</p>
+            {movie.poster_path && (
+              <img
+                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                alt={movie.title}
+              />
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
